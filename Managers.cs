3b366d8d94fb66b3bf6fcddab21767288264a999
bongdeng9 <80138709+bongdeng9@users.noreply.github.com>
2022-08-02 14:01:33 +0900

    Add files via upload

---
+++ Managers.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+
+    static Managers Instance; // 유일성이 보장된다. 전역으로 사용.
+    public static Managers GetInstance() { Init(); return Instance; } // 유일한 매니저를 갖고온다.
+
+
+    void Start()
+    {
+        Init();
+    }
+    void Update()
+    {
+        
+    }
+
+    static void Init()
+    {
+        if (Instance == null)
+        {
+            GameObject go = GameObject.Find("@Managers");
+            if (go == null)
+            {
+                go = new GameObject { name = "@Managers" };
+                go.AddComponent<Managers>();
+            }
+
+            DontDestroyOnLoad(go);
+            Instance = go.GetComponent<Managers>();
+        }
+    }
+}
---
+++ Plaayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plaayer : MonoBehaviour
+{
+    void Start()
+    {
+        // Managers mg = new Managers(); 와 똑같은 기능임. 근데 유니티에 있는거만 사용해야 되므로,
+        // 일반적인 문법으로는 불가하다. 그래서 이 방법을 사용한다.
+        // 게임 오브젝트를 이름으로 찾는거 자체가 부하가 심함. -> 싱글톤
+
+        Managers mg = Managers.GetInstance();
+        
+
+    }
+    void Update()
+    {
+        
+    }
+}
